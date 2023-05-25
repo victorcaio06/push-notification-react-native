b6c5549e131c30e62c7d144c44ae9a26ec7f2e55
Victor Caio <kaioaraujo426@gmail.com>
2023-05-25 09:01:31 -0300

    FEAT: added One Signal

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.setAppId('3db21c31-833f-4d8b-b339-07260a771446');
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
